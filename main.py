446e4c03adf43969ff4918e6b52fa5a2eb736c96
JohnnyOzark <jrogerowen@protonmail.com>
2025-04-24 15:35:24 -0700

    main now counts the words in a document

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ def get_book_text(file_path):
         print(f"error: the file at '{file_path}' was not found.")
         return None
 
+def book_word_count(book_text):
+    words = book_text.split()
+    word_count = len(words)
+    return word_count
+
 def main():
     file_path = "books/frankenstein.txt"
     book_text = get_book_text(file_path)
     if book_text:
-        print (book_text)
+        word_count = book_word_count(book_text)
+        print (f"{word_count} words found in the document")
     else:
         print("Could not read the book text. Please check the file path.")
 
